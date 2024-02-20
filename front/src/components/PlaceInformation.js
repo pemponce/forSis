9b4ee948a034ea7707bd91a3b3673a5e4d975986
VallessGIT <vadim.nigm@yandex.ru>
2024-02-20 18:13:24 +0300

    Шма

--- front/src/components/PlaceInformation.js
+++ front/src/components/PlaceInformation.js
@@ -25,7 +25,10 @@ export default function PlaceInformation() {
                         Ул. Техническая, 4б
                     </h3>
 
-                    <div style={{width:"600px", height:"400px"}}></div>
+                    <div style={{width: "600px", height: "400px", margin: "auto"}}>
+                        {/*55.764343, 49.130068*/}
+                        <iframe style={{borderRadius: 15}} src="https://yandex.ru/map-widget/v1/-/CDBT4B17" width="560" height="400" frameBorder="0" allowFullScreen="true"></iframe>
+                    </div>
                 </Box>
             </Paper>
         </Container>
